1de11136dc7a19a0ee5038bbff45dcf8044fdb1b
pat-s <patrick.schratz@gmail.com>
2019-08-27 09:18:27 +0200

    add day3_stats1 material

---
+++ 3_day/2_stats_I/R/B01_rsaga_landslides.R
@@ -0,0 +1,199 @@
+####################################################
+# Environmental Statistics and GeoComputation
+# GEO 408 FSU Jena
+# Alexander Brenning and Jason Goetz
+# Date: Winter 2019
+####################################################
+# Introduction to RSAGA
+# Case study: landslides in Ecuador
+####################################################
+
+# install.packages(c('RSAGA', 'here'))
+library(RSAGA)
+library(here)
+library(fs)
+
+# Please open '2_stats_I.Rproj' in RStudio. Package 'here' will then take care
+# of all file path handling throughout the session for you.
+
+##############################################
+# Calculate terrain attributes using (R)SAGA
+##############################################
+## RSAGA is better supported with versions <= 2.1.0
+
+# Let RSAGA find SAGA GIS:
+# env = rsaga.env()
+# If it can't find it, use the path argument to specify its location.
+# On my computer:
+# env = rsaga.env(path="C:/Progra~1/SAGA-GIS")
+# or
+# env = rsaga.env(path="C:/Program Files (x86)/SAGA-GIS")
+# Usually, the automatic search should be able to find your installation.
+env <- rsaga.env()
+
+# See what you've got:
+env
+
+#### Skip this because it's slow:
+# Explore available libraries and modules (slow!):
+modules <- rsaga.get.modules(env = env)
+names(modules)
+# We are going to use modules from the following libraries:
+modules$io_grid
+modules$ta_morphometry
+modules$ta_hydrology
+#### End Skip
+
+# Convert the DEM ASCII grid to SAGA grid format (.sgrd):
+rsaga.esri.to.sgrd(here("R/data/dem.asc"), env = env)
+
+# To be able to see what SAGA_CMD call was actually run, we
+# can enable the display.command argument:
+rsaga.esri.to.sgrd(here("R/data/dem.asc"), env = env, display.command = TRUE)
+
+# Or, we could do the same thing 'the long way', using
+# the low-level rsaga.geoprocessor:
+rsaga.get.usage("io_grid", "Import ESRI Arc/Info Grid", env = env)
+rsaga.geoprocessor("io_grid", "Import ESRI Arc/Info Grid",
+  param = list(GRID = "dem.sgrd", FILE = here("R/data/dem.asc")),
+  display.command = TRUE, env = env
+)
+# ...but we'd rather prefer the more convenient higher-level front-end
+# function in RSAGA...
+
+
+# Now let's calculate a bunch of terrain attributes:
+
+# Slope, aspect, plan and profile curvature:
+rsaga.slope.asp.curv(here("R/data/dem"),
+  out.slope = "slope", out.cplan = "plancurv",
+  out.cprof = "profcurv", method = "poly2zevenbergen", env = env
+)
+## SAGA versions older than 2.1.1 use this instead of rsaga.slope.asp.curv:
+## rsaga.local.morphometry("dem", out.slope = "slope",
+##        out.hcurv = "plancurv", out.vcurv = "profcurv",
+##        method = "poly2zevenbergen", env=env)
+# Create a hydrologically more meaningful DEM by filling any local sinks:
+rsaga.sink.removal("dem", out.dem = "sdem", method = "fill", env = env)
+# Use the multiple flow direction algorithm to calculate catchment area and slope:
+rsaga.parallel.processing("sdem",
+  out.carea = "carea",
+  out.cslope = "cslope", method = "mfd", env = env
+)
+
+# Convert terrain attribute grids into ASCII format:
+rsaga.sgrd.to.esri(c(
+  here("R/data/slope.asc"), here("R/data/plancurv.asc"),
+  here("R/data/profcurv.asc"), here("R/data/carea.asc"),
+  here("R/data/cslope.asc")
+), env = env)
+
+# Delete SAGA grids, we don't need them any more, unless you want to view them in SAGA GIS:
+file_delete(here(dir_ls("R/data/", glob = "*.mgrd")))
+file_delete(here(dir_ls("R/data/", glob = "*.sgrd")))
+file_delete(here(dir_ls("R/data/", glob = "*.sdat")))
+
+##############################################
+# Prepare the training and test samples:
+# Create random point sample and pick values
+# of predictor variables from grids
+##############################################
+
+# Read grid header --> find out the extent
+hdr <- read.ascii.grid.header(here("R/data/dem"))
+
+# Draw a (stratified) random sample of 500 landslide and 500 non-landslide
+# grid points from the inventory:
+N <- 25000
+# Start with 25000 samples, this sample should be large enough
+# to find enough landslide and non-landslide observations:
+d <- data.frame(
+  x = sample(hdr$xllcenter + c(0:(hdr$ncols - 1)) * hdr$cellsize,
+    size = N, replace = TRUE
+  ),
+  y = sample(hdr$yllcenter + c(0:hdr$nrows - 1) * hdr$cellsize,
+    size = N, replace = TRUE
+  )
+)
+
+# Pick the value of the 'mask' grid for each potential sample location:
+d <- pick.from.ascii.grid(d, file = here("R/data/mask"))
+# Note that this doesn't use SAGA GIS, therefore no 'env' argument!
+
+# Remove points that are outside the study area:
+d <- d[ !is.na(d$mask), ]
+d$mask <- NULL
+
+# Get the landslide presence/absence information from the inventory:
+d <- pick.from.ascii.grid(d, file = here("R/data/slides89"))
+d$slides89 <- factor(!is.na(d$slides89))
+
+# Take an equal number of landslide and non-landslide grid cells:
+N.sli <- sum(d$slides89 == "TRUE")
+sel <- c(sample(which(d$slides89 == "TRUE"), size = N.sli), sample(which(d$slides89 != "TRUE"), size = N.sli))
+d <- d[sel, ]
+
+# Pick values of predictor variables at sample locations:
+d <- pick.from.ascii.grids(d, file = here(c(
+  "R/data/slope.asc", "R/data/plancurv.asc",
+  "R/data/profcurv.asc", "R/data/carea.asc",
+  "R/data/cslope.asc", "R/data/distroad.asc",
+  "R/data/distdeforest.asc"
+)), quiet = FALSE)
+summary(d)
+
+# This was time consuming... better save the results in the data folder:
+#### save(d, file = "landslides.Rd", compress = TRUE)
+
+
+# Before we start doing more serious landslide modeling, let's just
+# find out how we can use RSAGA to apply a statistical model to
+# predict landslide susceptibility based on a stack of grids:
+fit <- glm(slides89 ~ slope + distroad, family = binomial, data = d)
+# It worked, but let's not trust the model's p-values, they are biased
+# because of spatial autocorrelation:
+summary(fit)
+
+# Apply this preliminary model on a pixel-by-pixel basis to a stack of
+# ASCII raster files with equal extent and resolution:
+multi.local.function(
+  in.grids = here(c("R/data/slope.asc", "R/data/distroad.asc")), out.varnames = "glmpred_prelim",
+  fun = grid.predict, fit = fit, control.predict = list(type = "response"),
+  quiet = FALSE
+)
+
+# multi.local.function has several siblings (local.function, focal.function
+# and multi.focal.function) that apply user-defined R functions to local
+# moving windows.
+# Just to give you a more or less meaningful example, to apply a maximum
+# filter over a 3-pixel radius circular moving window to get a more
+# 'conservative' landslide susceptibility map, we could do this:
+focal.function(
+  in.grid = "glmpred_prelim", varnames = "glmpred_prelim_max",
+  radius = 3, search.mode = "circle", fun = max,
+  mw.to.vector = TRUE, mw.na.rm = TRUE
+)
+
+# Instead of 'max', we could use a user-defined function such as:
+my.max <- function(x) {
+  # A not-so-smart way of implenenting 'max' manually:
+  return(sort(na.omit(as.vector(x)), decreasing = TRUE)[1])
+}
+focal.function(
+  in.grid = "glmpred_prelim", varnames = "glmpred_prelim_mymax",
+  radius = 3, search.mode = "circle", fun = my.max
+)
+
+# That's all for now... Now you have a general overview of the
+# capabilities of the RSAGA package for geoprocessing!
+
+## Make a 'nice' displace of your results
+library(raster)
+hillshade <- raster("hillshade.tif")
+pred <- raster("glmpred_prelim.asc")
+plot(hillshade,
+  col = gray.colors(10, start = 0.3, end = 0.9, gamma = 2.2, alpha = NULL),
+  legend = FALSE, main = "Landslide Susceptibility"
+)
+plot(pred, alpha = 0.60, add = TRUE)
+points(d, cex = 0.5)
---
+++ 3_day/2_stats_I/R/B02_statistical_learning_landslides.R
@@ -0,0 +1,282 @@
+####################################################
+# Environmental Statistics and GeoComputation
+# GEO 408 FSU Jena
+# Alexander Brenning and Jason Goetz
+# Date: Winter 2019
+####################################################
+# Statistical learning for classification
+# Case study: landslides in Ecuador
+####################################################
+
+
+##############################################
+# Prepare the training and test samples:
+# Transform some of the variables
+##############################################
+
+
+# This is where my raster data from Ecuador is located:
+setwd("D:/ecuador/data")
+
+library(ROCR)
+library(gam)
+library(rpart)
+library(randomForest)
+
+# Load the saved training/test data:
+(load("landslides.Rd"))
+
+# A function for plotting ROC curves and
+# calculating the area under the ROC curve (AUROC)
+# using the ROCR package:
+auroc <- function(pred, obs, plot = FALSE) {
+  stopifnot(is.logical(obs) | is.factor(obs))
+  stopifnot(is.numeric(pred))
+  stopifnot(length(pred) == length(obs))
+  if (is.factor(obs)) stopifnot(nlevels(obs) == 2)
+  require(ROCR)
+  predobj <- prediction(pred, obs)
+  if (plot) plot(performance(predobj, "tpr", "fpr"))
+  auroc <- performance(predobj, measure = "auc")@y.values[[1]]
+  return(auroc)
+}
+
+
+# Apply some transformations to the data:
+my.trafo <- function(x) {
+  # Same for >300m from deforestation:
+  x$distdeforest[ x$distdeforest > 300 ] <- 300
+  # ...and >300m from road:
+  x$distroad[ x$distroad > 300 ] <- 300
+  # Convert radians to degrees:
+  x$slope <- x$slope * 180 / pi
+  x$cslope <- x$cslope * 180 / pi
+  # Log-transform size of catchment area - it is extremely skewed:
+  x$log.carea <- log10(x$carea)
+  return(x)
+}
+
+d <- my.trafo(d)
+
+
+##############################################
+# Exploratory Data Analysis
+##############################################
+
+# Spinograms are a very fine tool for displaying
+# conditional probabilities:
+par(mfrow = c(2, 4), mex = 0.7, mar = c(5, 4, 2, 3))
+with(d, {
+  # spineplot(slides89 ~ distslidespast, ylevels = c("TRUE", "FALSE"), xlab = "Dist. from past landslides", ylab = "", yaxlabels = c("Landslide", "No lsl."),
+  #        breaks = c(0,10,40,80,100))
+  spineplot(slides89 ~ slope, ylevels = c("TRUE", "FALSE"), xlab = "Slope angle", ylab = "", yaxlabels = c("Landslide", "No landsl."))
+  spineplot(slides89 ~ plancurv, ylevels = c("TRUE", "FALSE"), xlab = "Plan curvature", ylab = "", yaxlabels = c("Landslide", "No landsl."))
+  spineplot(slides89 ~ profcurv, ylevels = c("TRUE", "FALSE"), xlab = "Profile curvature", ylab = "", yaxlabels = c("Landslide", "No landsl."))
+  spineplot(slides89 ~ log.carea, ylevels = c("TRUE", "FALSE"), xlab = "Log. contributing area", ylab = "", yaxlabels = c("Lsl.", "No landslide"))
+  spineplot(slides89 ~ cslope, ylevels = c("TRUE", "FALSE"), xlab = "Catchment slope", ylab = "", yaxlabels = c("Landslide", "No landsl."))
+  spineplot(slides89 ~ distdeforest,
+    ylevels = c("TRUE", "FALSE"), xlab = "Dist. to deforestation", ylab = "", yaxlabels = c("Landslide", "No lsl."),
+    breaks = c(0, 25, 50, 100, 250, 300)
+  )
+  spineplot(slides89 ~ distroad,
+    ylevels = c("TRUE", "FALSE"), xlab = "Distance to road", ylab = "", yaxlabels = c("Landslide", "No lsl."),
+    breaks = c(0, 25, 50, 100, 250, 300)
+  )
+})
+
+
+
+##############################################
+# We won't have time to run all of the following models,
+# so please pick only ONE of them.
+# The following models are available below:
+# - Generalized additive model (GAM) - my favourite!
+# - Classification tree (CART)
+# - Random forest
+##############################################
+
+
+##############################################
+# Generalized Additive Model
+##############################################
+
+# Fit a generalized additive model (GAM) with a logistic
+# link function by stepwise forward variable selection.
+# GAMs are semi-parametric models that combine linear and
+# nonlinear terms; they are more flexible than the generalized
+# linear model, but still interpretable because of their
+# additive structure.
+
+library(gam)
+# Predictor variables may be included as linear terms,
+# nonlinear smoothing spline terms with two equivalent
+# degrees of freedom, or not at all:
+gam.scope <- list(
+  ~ 1 + distroad + s(distroad, 2),
+  ~ 1 + I(distdeforest > 0) + distdeforest + s(distdeforest, 2),
+  ~ 1 + log.carea + s(log.carea, 2),
+  ~ 1 + cslope + s(cslope, 2),
+  ~ 1 + plancurv + s(plancurv, 2),
+  ~ 1 + profcurv + s(profcurv, 2),
+  ~ 1 + slope + s(slope, 2)
+)
+
+# Model building by stepwise variable selection based on the AIC:
+fit <- step.Gam(gam(slides89 ~ 1,
+  data = d,
+  family = binomial
+),
+scope = gam.scope,
+direction = "forward", trace = TRUE
+)
+
+summary(fit)
+
+# Plot the transformation functions
+# (linear relationship: linear term, no smoothing splines applied)
+par(mfrow = c(2, 3), mex = 0.7, mar = c(5, 5, 2, 2))
+plot(fit, residuals = FALSE, se = TRUE)
+# Compare to Brenning (2008) - stepwise variable selection will
+# *a* good model, but there may be other good models around...!
+# Stepwise procedures are not very robust - small changes to the
+# learning sample may completely change the final model.
+
+
+# Create ROC plots and calculate AUROC on the training set:
+par(mfrow = c(1, 1))
+pred <- predict(fit, newdata = d, type = "response")
+auroc(pred, d$slides89 == "TRUE", plot = TRUE)
+
+# Apply fitted GAM to the entire study area:
+library(RSAGA)
+multi.local.function(
+  in.grids = c("slope", "carea", "cslope", "plancurv", "profcurv", "distroad", "distdeforest"),
+  out.varnames = "gampred",
+  fun = grid.predict, control.predict = list(type = "response"),
+  fit = fit, trafo = my.trafo, quiet = FALSE
+)
+
+# Plot prediction map
+library(raster)
+gam.raster <- raster("gampred.asc")
+plot(gam.raster)
+
+##############################################
+# Classification Tree
+##############################################
+
+library(rpart)
+fit <- rpart(slides89 ~ slope + plancurv + profcurv +
+  log.carea + cslope + distdeforest + distroad,
+data = d,
+control = rpart.control(cp = 0.01)
+)
+par(xpd = TRUE, mfrow = c(1, 1))
+plot(fit)
+text(fit, use.n = TRUE)
+
+# Training set AUROC:
+pred <- predict(fit, newdata = d, type = "prob")[, "TRUE" ]
+auroc(pred, d$slides89 == "TRUE", plot = TRUE)
+
+multi.local.function(
+  in.grids = c("slope", "plancurv", "profcurv", "carea", "cslope", "distroad", "distdeforest"),
+  out.varnames = "ctpred",
+  fun = grid.predict, control.predict = list(type = "prob"),
+  fit = fit, trafo = my.trafo, predict.column = "TRUE",
+  quiet = FALSE
+)
+
+ct.raster <- raster("ctpred.asc")
+plot(ct.raster)
+
+
+##############################################
+# Random Forest
+##############################################
+
+library(randomForest)
+fit <- randomForest(slides89 ~ slope + plancurv + profcurv + log.carea + cslope +
+  distdeforest + distroad, data = d, importance = TRUE)
+fit
+
+# Variable importance plot:
+varImpPlot(fit, type = 1)
+
+# Training set AUROC:
+pred <- predict(fit, newdata = d, type = "prob")[, "TRUE" ]
+auroc(pred, d$slides89 == "TRUE", plot = TRUE)
+# AUROC: 1.00!!! (i.e. nicely overfitting...)
+
+
+multi.local.function(
+  in.grids = c("slope", "plancurv", "profcurv", "carea", "cslope", "distroad", "distdeforest"),
+  out.varnames = paste("rfpred", sep = ""),
+  fun = grid.predict, control.predict = list(type = "prob"),
+  fit = fit, trafo = my.trafo, predict.column = "TRUE",
+  quiet = FALSE
+)
+
+library(raster)
+rf.raster <- raster("rfpred.asc")
+plot(rf.raster)
+
+#####################################################
+# Apply prediction alternative with raster package
+#####################################################
+library(raster)
+
+# Load input rasters - can be other file types, e.g. tif, .img, ...
+plancurv <- raster("plancurv.asc")
+profcurv <- raster("profcurv.asc")
+
+# Don't forget the transformations we applied to the data
+log.carea <- log10(raster("carea.asc"))
+# or
+# carea <- raster('carea.asc')
+# log.carea <- calc(carea, fun= function(x){log10(x)})
+names(log.carea) <- "log.carea"
+
+rad.to.degree <- function(x) {
+  x * 180 / pi
+}
+slope <- calc(raster("slope.asc"), fun = rad.to.degree)
+names(slope) <- "slope"
+
+cslope <- calc(raster("cslope.asc"), fun = rad.to.degree)
+names(cslope) <- "cslope"
+
+distroad <- raster("distroad.asc")
+distroad[distroad > 300] <- 300
+
+distdeforest <- raster("distdeforest.asc")
+distdeforest[distdeforest > 300] <- 300
+
+
+# Stack grids
+layers <- stack(list(slope, log.carea, plancurv, profcurv, cslope, distroad, distdeforest))
+layers
+plot(layers)
+
+# Random forest example
+predfun.rf <- function(object, newdata) {
+  predict(object, newdata = newdata, type = "prob")[, 2]
+}
+
+rf <- raster::predict(layers, fit, fun = predfun.rf, progress = "text")
+writeRaster(rf, "pred_rf.img", format = "HFA", overwrite = TRUE)
+
+plot(rf)
+
+# Make nice plot of results
+hillshade <- raster("hillshade.tif")
+plot(hillshade,
+  col = gray.colors(10, start = 0.3, end = 0.9, gamma = 2.2, alpha = NULL),
+  legend = FALSE, main = "Landslide Susceptibility"
+)
+plot(rf, alpha = 0.60, add = TRUE)
+points(d, cex = 0.5)
+
+
+# This the end of an overview of the use of statistical learning techniques
+# for spatial modeling of a binary response variable!
